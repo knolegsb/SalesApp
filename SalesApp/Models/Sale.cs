a6af922c86d2c2d18026d5f9bb1dd82ba9ac2fea
knolegsb <seon2you@hotmail.com>
2016-01-30 00:44:42 -0800

    added models

---
+++ SalesApp/Models/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.Models
+{
+    class Sale : BaseModel
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
